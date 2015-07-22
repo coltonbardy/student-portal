1fc884ce405f45b8452f2b5577f4d6d9bf6deba8
Eric Johnson <ej2586@gmail.com>
2015-07-22 14:53:16 -0700

    added handling for resource search and add too today

--- sites/all/modules/lfz/misc/view-instructor-resources.js
+++ sites/all/modules/lfz/misc/view-instructor-resources.js
@@ -12,30 +12,44 @@ jQuery(document).ready(function () {
 
     jQuery('body').on('click', '.agenda-add-today', function () {
         console.log("click called");
+
+        //add spinner
+        var spinner_html = '<span class="spinner animate"><i class="glyphicon glyphicon-refresh"></i></span>';
+        jQuery(this).addClass("has-spinner");
+        if(jQuery(this).find(".spinner").length ==0){
+            jQuery(this).prepend(spinner_html);
+        }
+
+        jQuery(this).toggleClass('active');
+
         var item_nid = (jQuery(this).data('resource-nid'))?jQuery(this).data('resource-nid'):false;
 
         if(item_nid){
 
-            var url = base_path+'ajax/today/add-item';
+            (function(elm){
+                var url = base_path+'ajax/today/add-item';
 
-            var obj = {
-                'item_nid': item_nid,
-            };
+                var obj = {
+                    'nid': item_nid
+                };
 
-            jQuery.ajax({
-                url:url,
-                data:obj,
-                dataType:'json',
-                success:function(response){
-                    console.log("response : ", response);
-                    if(response.success){
-                    }else{
+                jQuery.ajax({
+                    url:url,
+                    data:obj,
+                    dataType:'json',
+                    success:function(response){
+                        console.log("response : ", response);
+                        if(response.success){
+                            elm.toggleClass('active');
+                        }else{
+                        }
+                    },
+                    error:function(response){
+
                     }
-                },
-                error:function(response){
+                });
+            })(jQuery(this));
 
-                }
-            });
         }else{
             //error getting nid
             console.error("couldn't get nid from element from : ", jQuery(this));
