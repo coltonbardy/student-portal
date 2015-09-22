9be1252ee6fc310a65b57f906b50ef5d7089634a
Eric Johnson <ej2586@gmail.com>
2015-09-22 15:24:09 -0700

    added calednar icon along with event type and direct link when its an event

--- sites/all/modules/lfz/templates/agenda-item-heading.tpl.php
+++ sites/all/modules/lfz/templates/agenda-item-heading.tpl.php
@@ -9,12 +9,24 @@ if ($links) {
     if (user_has_role(array_search('content manager', user_roles()))) {
         $edit = l('Edit', 'node/' . $nid . '/edit');
     }
+
+    $icon = "";
+
+    if($type === 'event'){
+        $icon = '<span class="glyphicon glyphicon-calendar glyphicon-align-left" aria-hidden="true">';
+
+        $direct_link = (count($node->field_reference_link) > 0)?$node->field_reference_link['und'][0]['value']:false;
+        if($direct_link){
+            $title = l($node->title, $direct_link, array("attributes"=>array("target"=>"_blank")));
+        }
+    }
 }
 
 ?>
 
 <div class="col-sm-8">
     <h5>
+        <?php echo $icon ; ?>
         <?php echo $title; ?>
         <?php if ($type): ?>
             -
