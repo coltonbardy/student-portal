7dbfb802088b754461cb9a96b41045298727b3d8
Eric Johnson <ej2586@gmail.com>
2015-12-29 14:34:36 -0800

    added links for agenda navigation

--- sites/all/modules/lfz/templates/info-session-email-template.tpl.php
+++ sites/all/modules/lfz/templates/info-session-email-template.tpl.php
@@ -6,16 +6,20 @@
  * Time: 12:26 PM
  */
 ?>
-Dear <?php print $name; ?>,
+Hello <?php print $name; ?>,
 
-We are pleased that you're coming to our Info-Session on <?php print $date; ?>. You will have an opportunity to mingle followed by a brief overview of the program and a Q and A session. It will conclude approximately an hour later, but please feel free to "hang-out" afterwards to talk with an instructor, the staff, current students or prospective students. The address is as follows:
+We are looking forward to meeting you at our Info Session on <?php print $date; ?> We will have a brief introduction followed by an overview of our program. Then, we will have the opportunity to discuss any of your questions.
+
+We estimate this will be approximately an hour. Afterwards, our instructors and staff will be available to go into further detail on an individual basis.
+
+We are located at:
 
 9080 Irvine Center Dr., 2nd Floor
 Irvine, CA 92618
 
-If you have any immediate questions now, we are available to speak with you over the phone M-F, from 9:00AM-5:00PM at (949) 679-7699. Alternatively, you can reach out to us via our Facebook page at http://www.facebook.com/learningfuze.com, or emailing us directly at support@learningfuze.com.
+Check out our Facebook page at  http://www.facebook.com/learningfuze.com to see a glimpse of what we’ll all about.
 
-We'll see you there!
+See you soon,
 
-LearningFuze Team
+The LearningFuze Team
 
--- sites/all/modules/lfz/templates/weekly-agenda.tpl.php
+++ sites/all/modules/lfz/templates/weekly-agenda.tpl.php
@@ -1,3 +1,6 @@
+<div class="col-xs-12">
+    <div id="navigation" class=""><?php echo $last_week_link; ?> - <?php echo $next_week_link; ?></div>
+</div>
 <?php
 foreach ($days as $d):
     ?>
